756292fe20ee984975038e767b2bc4547715a33f
TSAR-C <QixuanZhao@outlook.com>
2024-09-11 01:24:51 +0800

    Completed LSF (LSA)

---
+++ Phantom/Views/Functional Panels/Functional Pieces/B-Spline Curve/CurveSampler.swift
@@ -0,0 +1,18 @@
+//
+//  CurveSampler.swift
+//  Phantom
+//
+//  Created by TSAR Weasley on 2024/9/1.
+//
+
+import SwiftUI
+
+struct CurveSampler: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CurveSampler()
+}
---
+++ Phantom/Views/Functional Panels/Functional Pieces/B-Spline Surface/LSFSurfaceConstructor.swift
@@ -0,0 +1,18 @@
+//
+//  LSFSurfaceConstructor.swift
+//  Phantom
+//
+//  Created by TSAR Weasley on 2024/9/1.
+//
+
+import SwiftUI
+
+struct LSFSurfaceConstructor: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LSFSurfaceConstructor()
+}
